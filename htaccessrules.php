e336e82b3916694dc0e2b2004fad6a4cc9046f0d
Darren <you@example.comdarren@kandekore.net>
2023-08-01 19:29:05 +0100

    lables

--- htaccessrules.php
+++ htaccessrules.php
@@ -71,31 +71,96 @@ class My_Htaccess_Rules {
         $compression_enabled = get_option( self::OPTION_COMPRESSION, 0 );
         $caching_enabled = get_option( self::OPTION_CACHING, 0 );
         ?>
-           <div class="wrap">
+        <div class="wrap">
+        <style>
+    .switch {
+      position: relative;
+      display: inline-block;
+      width: 60px;
+      height: 34px;
+    }
+    
+    .switch input {
+      opacity: 0;
+      width: 0;
+      height: 0;
+    }
+    
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+    
+    .slider:before {
+      position: absolute;
+      content: "";
+      height: 26px;
+      width: 26px;
+      left: 4px;
+      bottom: 4px;
+      background-color: white;
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+    
+    input:checked + .slider {
+      background-color: #2196F3;
+    }
+    
+    input:focus + .slider {
+      box-shadow: 0 0 1px #2196F3;
+    }
+    
+    input:checked + .slider:before {
+      -webkit-transform: translateX(26px);
+      -ms-transform: translateX(26px);
+      transform: translateX(26px);
+    }
+    
+    /* Rounded sliders */
+    .slider.round {
+      border-radius: 34px;
+    }
+    
+    .slider.round:before {
+      border-radius: 50%;
+    }
+    </style>
             <h2>My Htaccess Rules Settings</h2>
             <form method="POST">
-                <label>
+                <label class="switch">
                     <input type="checkbox" id="main-switch" name="<?php echo self::OPTION_NAME; ?>" value="1" <?php checked( $enabled ); ?>>
-                    Enable Plugin
-                </label>
-                <p>Turn this on to enable the My Htaccess Rules plugin.</p>
+                    <span class="slider round"></span>
+                   
+                </label><h3> Enable Plugin</h3>
+                <p>Turn this on to enable the My Htaccess Rules plugin and create a backup of your .htaccess file (this backup will be restored upon deactivation of this plugin)</p>
                 <br>
-                <label>
+                <label class="switch">
                     <input type="checkbox" class="dependent-checkbox" name="<?php echo self::OPTION_COMPRESSION; ?>" value="1" <?php checked( $compression_enabled ); echo ($enabled ? '' : 'disabled'); ?>>
-                    Enable Compression Rules
-                </label>
-                <p>(Greyed out if above is not enabled) Compresses your site's HTML, CSS, JavaScript, and more to improve loading times.</p>
+                    <span class="slider round"></span>
+                   
+                </label><h3>Enable Compression</h3>
+                <p>Compresses your site's HTML, CSS, JavaScript, and more to improve loading times.</p>
                 <br>
-                <label>
+                <label class="switch">
                     <input type="checkbox" class="dependent-checkbox" name="<?php echo self::OPTION_CACHING; ?>" value="1" <?php checked( $caching_enabled ); echo ($enabled ? '' : 'disabled'); ?>>
-                    Enable Caching Rules
-                </label>
-                <p>(Greyed out if "Enable Plugin" is not enabled) Sets expiration times for certain types of content, reducing server load and improving performance.</p>
+                    <span class="slider round"></span>
+                  
+                </label><h3>  Enable Leverage Browser Caching</h3>
+                <p>Sets expiration times for certain types of content, reducing server load and improving performance.</p>
                 <?php submit_button(); ?>
             </form>
         </div>
         <?php
     }
+    
     function insert_rules() {
         $enabled = get_option( self::OPTION_NAME, 0 );
         if ( $enabled ) {
